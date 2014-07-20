871cd8cd3879153f02405a32e22dfbb561ace74d
goldford <gr336681@dal.ca>
2014-07-20 00:06:01 -0700

    FIPEX 10.23 ArcGIS 10.x
    Latest attempt at uploading FIPEX 10.23 with all accompanying documents.

---
+++ FiPEX_AddIn_dotNet35_2/VisualizeDecisions.vb
@@ -0,0 +1,26 @@
+﻿Public Class VisualizeDecisions
+        Inherits ESRI.ArcGIS.Desktop.AddIns.Button
+        Private m_FiPEx__1 As FishPassageExtension
+
+        Public Sub New()
+
+        End Sub
+
+        Protected Overrides Sub OnClick()
+            My.ArcMap.Application.CurrentTool = Nothing
+            If m_FiPEx__1 Is Nothing Then
+            m_FiPEx__1 = FiPEX_AddIn_dotNet35_v10_23.FishPassageExtension.GetExtension()
+        End If
+        Using MyForm As New FiPEX_AddIn_dotNet35_v10_23.frmVisualizeDecisions
+            If MyForm.Form_Initialize(My.ArcMap.Application) Then
+                MyForm.ShowDialog()
+            End If
+        End Using
+    End Sub
+
+    Protected Overrides Sub OnUpdate()
+        If m_FiPEx__1 Is Nothing Then
+            m_FiPEx__1 = FiPEX_AddIn_dotNet35_v10_23.FishPassageExtension.GetExtension()
+        End If
+    End Sub
+    End Class
